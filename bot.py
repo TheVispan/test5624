d5e49d62f7ed0857cf1651ee55cbb122fa7d1c4a
TheVispan <53561284+TheVispan@users.noreply.github.com>
2019-10-02 11:57:23 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 from  discord.ext import commands
 from discord.ext.commands import Bot
 owm = pyowm.OWM('cd1ce42897e0ce128723e6f7d8c20864')
-observation = owm.weather_at_place('London,GB')
+observation = owm.weather_at_place('Ekaterinburg, RU')
 w = observation.get_weather()
 
 Bot = commands.Bot(command_prefix= '!')
